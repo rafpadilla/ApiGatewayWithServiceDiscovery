eebb134477b6441f554cf74fb15b1d8b56df57f4
Rafael Padilla <rafpadilla@gmail.com>
2021-11-14 15:42:50 +0100

    change service with with hostname in main view

--- ApiGatewayWithServiceDiscovery.WebApplication/Controllers/HomeController.cs
+++ ApiGatewayWithServiceDiscovery.WebApplication/Controllers/HomeController.cs
@@ -1,5 +1,4 @@
-﻿using Consul;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using System.Net;
 
 namespace ApiGatewayWithServiceDiscovery.WebApplication.Controllers
@@ -7,21 +6,17 @@ namespace ApiGatewayWithServiceDiscovery.WebApplication.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-        private readonly IConsulClient _consulClient;
 
-        public HomeController(ILogger<HomeController> logger, IConsulClient consulClient)
+        public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
-            _consulClient = consulClient;
         }
 
-        public async Task<IActionResult> Index()
+        public IActionResult Index()
         {
-            var services = await _consulClient.Agent.Services();
             var hostname = Dns.GetHostName();
-            var serviceId = services.Response.FirstOrDefault(a => a.Value.Service.Equals("WebApp") && a.Value.Address.Equals(hostname));
 
-            return View(model: serviceId.Key ?? "0");
+            return View(model: hostname);
         }
         public IActionResult Error()
         {
